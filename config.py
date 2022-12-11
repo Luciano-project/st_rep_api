64c97e5a837a67afbb912bab81d5a26f32e84de4
Luciano Sousa <72676344+Luciano-project@users.noreply.github.com>
2022-12-11 23:21:24 +0000

    Update and rename main.py to config.py

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
 
-#Aplicar o token Replicate no ambiente virtual
-# && echo "Token aplicado ao ambiente virtual!"')
 
+# Arquivo de configuração inicial da aplicação.
+
+list_dir = os.listdir()
 # Carregará as configurações para a aplicação
 try:
     # Verificará se há o diretório static (onde ficarão os arquivos gerados por predições), caso não haja será criado o mesmo
     os.system(f'if [ ! -d static ]; then mkdir static && echo "Diretório {"static/"} criado com sucesso!"; fi')
     # Seleciona o path para salvar os arquivos
     save_path='static'
-    token_replicate = input('Insira seu API token Replicate: ')
-    os.system(f'export REPLICATE_API_TOKEN={token_replicate}; echo "Chave exportada com sucesso"')
-    os.system('python3 app.py')
+
+    if "venv" not in list_dir:
+        print("Criando ambiente virtual...")
+        os.system("python3 -m venv venv")
+        print("Ambiente virtual criado com sucesso!")
 except:
     print("Erro ao tentar criar diretório.")
